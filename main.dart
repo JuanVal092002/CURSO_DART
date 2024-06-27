2b4841849e06a728bda0bae4ca8b0e274d5e9675
JuanVal092002 <juancarlospastasvalencia@hotmail.com>
2024-06-27 12:18:42 -0500

    Imprimi desde consola mi primer Hola Mundo

---
+++ main.dart
@@ -0,0 +1,9 @@
+//  Imprimir strings en este caso Hola Mundo
+void main(){
+print("Hola Mundo");  
+}
+
+// Para hacer debugging en Dart usamos el siguinete comando
+
+// En este caso el archivo en el que estamos escribiendo el codigo se llama main.dart
+      // dart run main.dart
